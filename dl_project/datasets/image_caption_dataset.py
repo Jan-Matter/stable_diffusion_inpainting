8977a11d7c06581b7a08a369bc40f16a9214ebcc
Andras Geiszl <ageiszl@student.ethz.ch>
2023-12-27 04:33:32 +0100

    Add image caption dataset

--- dl_project/datasets/image_caption_dataset.py
+++ dl_project/datasets/image_caption_dataset.py
@@ -1,26 +1,56 @@
-from torch.utils.data import Dataset
+"""Module containing the Dataset for image captioning tasks."""
+import io
+import multiprocessing
+import urllib
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+
+import PIL.Image
+from datasets import load_dataset
+from datasets.utils.file_utils import get_datasets_user_agent
+from torchvision.datasets import LSUN, CelebA
+
+USER_AGENT = get_datasets_user_agent()
 
 
-class ImageCaptionDataset(Dataset):
+def fetch_single_image(image_url, timeout=None, retries=0):
+    for _ in range(retries + 1):
+        try:
+            request = urllib.request.Request(
+                image_url,
+                data=None,
+                headers={"user-agent": USER_AGENT},
+            )
+            with urllib.request.urlopen(request, timeout=timeout) as req:
+                image = PIL.Image.open(io.BytesIO(req.read()))
+            break
+        except Exception:
+            image = None
+    return image
 
-    def __init__(self):
-        pass
 
-    def __len__(self):
-        """
-        Returns:
-            Number of image caption pairs in the dataset
-        """
-        pass
+def fetch_images(batch, num_threads, timeout=None, retries=0):
+    fetch_single_image_with_args = partial(
+        fetch_single_image, timeout=timeout, retries=retries
+    )
 
-    def __getitem__(self, idx):
-        """
-        Args:
-            idx: index of the image caption pair
-        Returns:
-            {
-                image: tensor of shape (3, 512, 512)
-                caption: string should be around 5-10 words
-            }
-        """
-        pass
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        batch["image"] = list(
+            executor.map(fetch_single_image_with_args, batch["image_url"])
+        )
+
+    return batch
+
+
+conceptual_dataset = load_dataset("conceptual_captions")
+conceptual_dataset = conceptual_dataset.map(
+    fetch_images,
+    batched=True,
+    batch_size=100,
+    fn_kwargs={"num_threads": multiprocessing.cpu_count()},
+)
+
+conceptual_dataset.with_format("torch")
+
+celeba_dataset = CelebA("data/celeba")
+lsun_church_dataset = LSUN("data/lsun_church", "church")
