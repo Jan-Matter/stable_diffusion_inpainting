89d3693284b49e22d2302dee605f50740af1eaca
Yang Li <1284632889@qq.com>
2023-12-31 22:34:23 +0100

    added comments

--- dl_project/loss/test_contrastive_loss.py
+++ dl_project/loss/test_contrastive_loss.py
@@ -14,22 +14,29 @@ def main():
     # Parameters
     num_features = 8  # Number of vectors in the batch
     feature_length = 128  # Length of each vector
+
+    # construct dummy inputs
     group_indices = torch.tensor([0, 0, 0, 1, 1, 1, 2, 2], dtype=torch.int)
     features = torch.randn(num_features, feature_length).cuda()
 
+    # create contrastive loss instance
     ct_loss = ContrastiveLoss()
 
+    # naive implementation of contrastive loss
     loss = ct_loss(features, group_indices, reduce='mean')
     loss_matrix = ct_loss(features, group_indices, reduce='none')
 
+    # vectorized version of contrastive loss
     vectorized_loss = ct_loss.vectorized_forward(features, group_indices, reduce='mean')
     vectorized_loss_matrix = ct_loss.vectorized_forward(features, group_indices, reduce='none')
 
+    # print out losses
     print(loss)
     print(loss_matrix)
     print(vectorized_loss)
     print(vectorized_loss_matrix)
 
+    # check if naive and vectorized versions produce the same result
     print("loss and vectorized loss are the same:", (loss.item() - vectorized_loss.item()) < 1e-4)
     print("loss matrix and vectorized loss matrix are the same:", (torch.abs(loss_matrix - vectorized_loss_matrix)).max().item() < 1e-4)
 
